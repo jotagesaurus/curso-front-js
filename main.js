ebd655c3f2d6984577bbb25b7eccf7a27dea59b9
julian.guarin.arus <julian6732@gmail.com>
2023-03-01 16:02:50 -0500

    Menu mobile y aside order desktop

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 const menuEmail=document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const hambMenu=document.querySelector(".menu");
+const mobileMenu=document.querySelector(".mobile-menu");
+const myOrder=document.querySelector(".my-order")
+const cartIcon=document.querySelector(".navbar-shopping-cart img")
+const orderExit=document.querySelector(".title-container img")
 
 menuEmail.addEventListener('click',toggleMenu);
+hambMenu.addEventListener("click",toggleMobileMenu);
+cartIcon.addEventListener("click",toggleMyOrder);
+orderExit.addEventListener("click",toggleMyOrder);
 
 function toggleMenu(){
     desktopMenu.classList.toggle("inactive");
+}
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("inactive");
+}
+
+function toggleMyOrder(){
+    myOrder.classList.toggle("inactive");
 }
